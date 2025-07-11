4916d5a999a21249835344f8bec5c3b398784d07
EmiliaCabrer1 <emi.22692@gmail.com>
2025-07-11 10:11:10 -0300

    Card en small.

--- src/Componentes/Card.jsx
+++ src/Componentes/Card.jsx
@@ -1,67 +1,79 @@
 import { useState } from "react";
 import DetalleCard from "./DetalleCard";
 
-const Card = ({propiedad: propiedad, index: index}) => {
-  const [mostrarDetalle, setMostrarDetalle]= useState(false);
+const Card = ({ propiedad: propiedad, index: index }) => {
+  const [mostrarDetalle, setMostrarDetalle] = useState(false);
 
   const onClose = () => {
     setMostrarDetalle(false);
-  }
+  };
 
   return (
     <>
-    <div id={index} className="w-[90%] sm:w-[50%] h-50 bg-white rounded-lg shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] m-8 flex gap-8" onClick={() => setMostrarDetalle(!mostrarDetalle)}>
-      <div>
-        <img
-          className="w-75 h-53 object-cover rounded-lg shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] ml-2 mt-2"
-          src={propiedad.img}
-          alt="Img prueba"
-        />
+      <div
+        id={index}
+        className="w-[90%] sm:w-[50%] h-70 sm:h-50 bg-white rounded-lg shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] my-6 mx-4 sm:m-8 flex-col sm:flex gap-2 sm:gap-8"
+        onClick={() => setMostrarDetalle(!mostrarDetalle)}
+      >
+        <div className="relative sm:static">
+          <img
+            className="w-[105%] sm:w-[40%] h-30 sm:h-53 object-cover rounded-lg shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] ml-2 -mt-3 sm:mt-2 absolute sm:static "
+            src={propiedad.img}
+            alt="Img prueba"
+          />
+        </div>
+        <div className="w-full h-30 sm:w-48 sm:h-full sm:flex" id="1"></div>
+        <div className="mt-0 sm:mt-2 mr-2 justify-start text-blue-900 gap-2 px-3 sm:px-0 ">
+          <div className="flex items-center ">
+            <h2 className="text-base sm:text-lg font-semibold">
+              {propiedad.valor}
+            </h2>
+            <h3 className="text-sm">USD</h3>
+          </div>
+          <p className="text-xs sm:text-base my-1 sm:my-2">
+            {propiedad.descripcion}
+          </p>
+          <div className="flex items-center gap-4">
+            <div className="flex items-center gap-1">
+              <img src="/assets/m2-tarjeta.svg" alt="m2" />
+              <h6 className="text-xs sm:text-base ">{propiedad.m2} m2</h6>
+            </div>
+            <div className="flex items-center gap-1">
+              <img src="/assets/hab-tarjeta.svg" alt="hab" />
+              <h6 className="text-xs sm:text-base ">
+                {propiedad.habitaciones} Habs.
+              </h6>
+            </div>
+            <div className="flex items-center gap-1">
+              <img src="/assets/bano-tarjeta.svg" alt="baño" />
+              <h6 className="text-xs sm:text-base ">{propiedad.banos} Baño</h6>
+            </div>
+          </div>
+          <p className="my-2 text-sm sm:text-base font-semibold">
+            {propiedad.direccion}
+          </p>
+          <div className="flex items-center gap-2 sm:gap-4 mt-4 ">
+            <img
+              className="w-3 sm:w-4 h-3 sm:h-4"
+              src="/assets/wapp-tarjeta.svg"
+              alt="Whatsapp"
+            />
+            <img
+              className="w-3 sm:w-4 h-3 sm:h-4"
+              src="/assets/tel-tarjeta.svg"
+              alt="Llamada"
+            />
+            <img
+              className="w-3 sm:w-4 h-3 sm:h-4"
+              src="/assets/email-tarjeta.svg"
+              alt="Gmail"
+            />
+          </div>
+        </div>
       </div>
-      <div className="mt-2 mr-2 justify-start text-blue-900 gap-2">
-        <div className="flex items-center ">
-          <h2 className="text-lg font-semibold">{propiedad.valor}</h2>
-          <h3 className="text-sm">USD</h3>
-        </div>
-        <p className="my-2">
-{propiedad.descripcion}
-        </p>
-        <div className="flex items-center gap-4">
-          <div className="flex items-center gap-1">
-            <img src="/assets/m2-tarjeta.svg" alt="m2" />
-            <h6>{propiedad.m2} m2</h6>
-          </div>
-          <div className="flex items-center gap-1">
-            <img src="/assets/hab-tarjeta.svg" alt="hab" />
-            <h6>{propiedad.habitaciones} Habs.</h6>
-          </div>
-          <div className="flex items-center gap-1">
-            <img src="/assets/bano-tarjeta.svg" alt="baño" />
-            <h6>{propiedad.banos} Baño</h6>
-          </div>
-        </div>
-        <p className="my-2 text-base font-semibold">{propiedad.direccion}</p>
-        <div className="flex items-center gap-4 mt-4 ">
-          <img
-            className="w-4 h-4"
-            src="/assets/wapp-tarjeta.svg"
-            alt="Whatsapp"
-          />
-          <img
-            className="w-4 h-4"
-            src="/assets/tel-tarjeta.svg"
-            alt="Llamada"
-          />
-          <img
-            className="w-4 h-4"
-            src="/assets/email-tarjeta.svg"
-            alt="Gmail"
-          />
-        </div>
-      </div>
-    </div>
-    {mostrarDetalle && (
-        <DetalleCard propiedad={propiedad} onClose={onClose} />)}
+      {mostrarDetalle && (
+        <DetalleCard propiedad={propiedad} onClose={onClose} />
+      )}
     </>
   );
 };
